6d5a907c83fbf0bb400f75b47c6f6e851a5db9a2
DelgadoSamantha <itziasamantha19@gmail.com>
2023-06-04 01:33:51 -0600

    Diseño de interfaz

--- codificacion/src/main/java/frames/Chat.java
+++ codificacion/src/main/java/frames/Chat.java
@@ -7,10 +7,15 @@ package frames;
 import com.mycompany.codificacion.Codificacion;
 import com.mycompany.codificacion.Time;
 import com.mycompany.codificacion.Encriptation;
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontFormatException;
+import java.awt.Image;
 import java.awt.RenderingHints.Key;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.DataInputStream;
 import java.io.File;
@@ -23,11 +28,17 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Hashtable;
 import java.util.logging.Level;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import sockets.ConfigurationWindow;
@@ -57,9 +68,117 @@ public class Chat extends javax.swing.JFrame {
         this.setMinimumSize(new Dimension(640,240));
         this.setLocationRelativeTo(null);
         this.btnDicTok.setVisible(false);
+        
+        
+        
+//        JTableHeader header = encoding.getTableHeader();
+//        header.setDefaultRenderer(new CustomHeaderRenderer());
 
+        encoding.getTableHeader().setBackground(Color.BLUE);
+        
+        Font customFont = loadFont("src/main/java/fonts/PressStart2P-Regular.ttf", Font.PLAIN, 12);
+        mensaje.setFont(customFont);
+        enviar.setFont(customFont);
+        jLabel2.setFont(customFont);
+        messagesToShow.setFont(customFont);
+        encoding.setFont(customFont);
+        mensajeCodigos.setFont(customFont);
+        suffleReverse.setFont(customFont);
+        decodingTable.setFont(customFont);
+        numeroVueltas.setFont(customFont);
+        time.setFont(customFont);
+        //Color colorFuente = Color.green;
+        btnDicTok.setFont(customFont);
+        
+        enviar.setPreferredSize(new Dimension(100, 50));
+        enviar.setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
+        enviar.setBackground(Color.green);
+        enviar.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                
+            }
+        });
+        
+        enviar.setRolloverEnabled(true);
+        enviar.getModel().addChangeListener(e -> {
+            ButtonModel model = (ButtonModel) e.getSource();
+            if (model.isRollover()) {
+                enviar.setBackground(Color.BLACK);
+                enviar.setForeground(Color.WHITE);
+            } else {
+                enviar.setBackground(Color.GREEN);
+            }
+        });
+        
+        enviar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        
+        ImageIcon iconoenviar = new ImageIcon("src/main/java/img/enviar.png");
+        //enviar.setIcon(iconoenviar);
+        Image imagen = iconoenviar.getImage();
+
+        int anchoImagen = 80;
+        int altoImagen = 50;
+
+        Image imagenRedimensionada = imagen.getScaledInstance(anchoImagen, altoImagen, Image.SCALE_SMOOTH);
+        ImageIcon iconoRedimensionado = new ImageIcon(imagenRedimensionada);
+        enviar.setIcon(iconoRedimensionado);
+        enviar.setPreferredSize(new Dimension(anchoImagen, altoImagen));
+        
+        // Crear un borde con margen alrededor de la imagen
+        //int margen = 5;
+//        enviar.setBorder(BorderFactory.createCompoundBorder(
+//            new EmptyBorder(margen, margen, margen, margen),
+//            enviar.
+//        ));
+        enviar.setVerticalTextPosition(JButton.CENTER);
+        enviar.setHorizontalTextPosition(JButton.CENTER);
+        
+        //-------------------------------------------
+        
+         // Establecer el tamaño preferido del botón
+        //btnDicTok.setPreferredSize(new Dimension(50, 30));
+
+        btnDicTok.setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
+        
+        
+        btnDicTok.setBackground(Color.green);
+        btnDicTok.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                
+            }
+        });
+        
+        btnDicTok.setRolloverEnabled(true);
+        btnDicTok.getModel().addChangeListener(e -> {
+            ButtonModel model = (ButtonModel) e.getSource();
+            if (model.isRollover()) {
+                btnDicTok.setBackground(Color.BLACK);
+                btnDicTok.setForeground(Color.WHITE);
+            } else {
+                btnDicTok.setBackground(Color.GREEN);
+            }
+        });
+        
+        btnDicTok.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        
     }
-
+    
+    private Font loadFont(String path, int style, int size) {
+        try {
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File(path));
+            return customFont.deriveFont(style, size);
+        } catch (FontFormatException | IOException ex) {
+            ex.printStackTrace();
+            // Manejar el error de carga de la fuente
+            return null; // o puedes utilizar una fuente de respaldo
+        }
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -99,7 +218,7 @@ public class Chat extends javax.swing.JFrame {
         setTitle("Chat ");
 
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 255, 0), 10), "Chat", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 255, 0), 10), "CHAT ENCRYPT", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Bodoni MT Black", 1, 12), new java.awt.Color(255, 255, 255))); // NOI18N
         jPanel1.setAutoscrolls(true);
         jPanel1.setLayout(new java.awt.BorderLayout());
 
@@ -112,16 +231,20 @@ public class Chat extends javax.swing.JFrame {
         messagesToShow.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         messagesToShow.setForeground(new java.awt.Color(51, 255, 51));
         messagesToShow.setRows(5);
-        messagesToShow.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createTitledBorder(""), "Messages", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+        messagesToShow.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createTitledBorder(""), "MESSAGES", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Bodoni MT Black", 1, 10), new java.awt.Color(255, 255, 255))); // NOI18N
         jScrollPane1.setViewportView(messagesToShow);
 
         jPanel4.add(jScrollPane1);
 
         jPanel6.setLayout(new javax.swing.BoxLayout(jPanel6, javax.swing.BoxLayout.PAGE_AXIS));
 
-        jPanel8.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Encoding", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+        jPanel8.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel8.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "ENCODING", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Bodoni MT Black", 1, 10), new java.awt.Color(255, 255, 255))); // NOI18N
         jPanel8.setLayout(new javax.swing.BoxLayout(jPanel8, javax.swing.BoxLayout.PAGE_AXIS));
 
+        encoding.setBackground(new java.awt.Color(0, 0, 0));
+        encoding.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        encoding.setForeground(new java.awt.Color(51, 255, 0));
         encoding.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -133,10 +256,13 @@ public class Chat extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        encoding.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         jScrollPane4.setViewportView(encoding);
 
         jPanel8.add(jScrollPane4);
 
+        mensajeCodigos.setBackground(new java.awt.Color(0, 0, 0));
+        mensajeCodigos.setForeground(new java.awt.Color(51, 255, 0));
         mensajeCodigos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -154,9 +280,12 @@ public class Chat extends javax.swing.JFrame {
 
         jPanel6.add(jPanel8);
 
-        jPanel9.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Decoding", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+        jPanel9.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel9.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "DECODING", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Bodoni MT Black", 1, 10), new java.awt.Color(255, 255, 255))); // NOI18N
         jPanel9.setLayout(new javax.swing.BoxLayout(jPanel9, javax.swing.BoxLayout.PAGE_AXIS));
 
+        suffleReverse.setBackground(new java.awt.Color(0, 0, 0));
+        suffleReverse.setForeground(new java.awt.Color(51, 255, 0));
         suffleReverse.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -172,6 +301,8 @@ public class Chat extends javax.swing.JFrame {
 
         jPanel9.add(jScrollPane5);
 
+        decodingTable.setBackground(new java.awt.Color(0, 0, 0));
+        decodingTable.setForeground(new java.awt.Color(51, 255, 0));
         decodingTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -199,7 +330,6 @@ public class Chat extends javax.swing.JFrame {
         jPanel3.setPreferredSize(new java.awt.Dimension(136, 66));
         jPanel3.setLayout(new java.awt.BorderLayout());
 
-        enviar.setText("Enviar");
         enviar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         enviar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -212,7 +342,7 @@ public class Chat extends javax.swing.JFrame {
         mensaje.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         mensaje.setForeground(new java.awt.Color(0, 255, 0));
         mensaje.setToolTipText("");
-        mensaje.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createTitledBorder(""), "Ingresa mensaje", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+        mensaje.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createTitledBorder(""), "INGRESA EL MENSAJE", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Bodoni MT Black", 1, 10), new java.awt.Color(255, 255, 255))); // NOI18N
         mensaje.setCaretColor(new java.awt.Color(255, 255, 255));
         mensaje.setPreferredSize(new java.awt.Dimension(64, 66));
         mensaje.addActionListener(new java.awt.event.ActionListener() {
@@ -277,6 +407,8 @@ public class Chat extends javax.swing.JFrame {
         System.out.println("mensaje: "+this.mensaje.getText());
         //Font font = new Font("Orbitron-Medium", Font.PLAIN, 20);
         //enviar.setFont(font);
+        
+        
     }//GEN-LAST:event_enviarActionPerformed
 
     private void mensajeKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_mensajeKeyReleased
@@ -300,6 +432,8 @@ public class Chat extends javax.swing.JFrame {
         String textTrash = Encriptation.textTrash;*/
         //c = Encriptation.getDicc();
         new DiccionarioToken(Encriptation.getDicc(), vecPosTrash, vecDirTrash, textTrash, this.ciclos).setVisible(true);
+        
+        
         
     }//GEN-LAST:event_btnDicTokMouseClicked
     private void inicia(){
@@ -349,16 +483,12 @@ public class Chat extends javax.swing.JFrame {
         t = new Time(c.getTimeLabel());
         t.start();
         
-        File fuente=new File("src/main/java/fonts/Courgette-Regular.ttf");
-        Font font=null; 
-        try {
-            font = Font.createFont(Font.TRUETYPE_FONT,fuente);
-        } catch (FontFormatException ex) {
-            java.util.logging.Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            java.util.logging.Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
+        
+        
+        
         c.recibirMensajesServidor();
+        
         
         
         
@@ -404,7 +534,7 @@ public class Chat extends javax.swing.JFrame {
         //this.jTextArea1.add(scrollPane);
         //this.jScrollPane2.setPreferredSize(this.jTextArea1.getPreferredScrollableViewportSize());
 
-
+       
     }
     public void recibirMensajesServidor(){
         // Obtiene el flujo de entrada del socket
